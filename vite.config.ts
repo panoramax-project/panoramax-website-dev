35006bf15b972b1c5439247385f79d030254594a
Andreani Jean <ja.andreani@gmail.com>
2023-10-24 14:32:24 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/panoramax-project.github.io/panoramax-website-dev/',
+  base: "https://panoramax-project.github.io/panoramax-website-dev/",
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
